76794f6eb1d1506cd4977a3d368a4d5a64a9b037
Taylor Floyd <taylor.mackenzie.floyd@gmail.com>
2023-01-23 12:19:23 -0500

    Updated app text, logo, and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './icons8-weather.svg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,33 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Weather Application
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://weather.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See Current Weather
         </a>
+
       </header>
+      <footer className="App-footer">
+      <a 
+          className="App-logo-link"
+          target="_blank" 
+          href="https://icons8.com/icon/qA3w9Yp2vY7r/weather">
+          Weather
+        </a>
+        <p>icon by</p>
+        <a
+        className="App-logo-link"
+        target="_blank"
+        href="https://icons8.com">
+          Icons8
+        </a>
+      </footer>
     </div>
   );
 }
